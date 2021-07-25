41e201fd8cf8aa2eaa66ce6574208afb71a5c12d
RebelliousUno <1047313+RebelliousUno@users.noreply.github.com>
2021-07-25 13:22:34 +0100

    Fix Furnace Fuel for 1.17.1

--- src/main/java/uno/rebellious/lavasponge/blocks/BlockRegister.java
+++ src/main/java/uno/rebellious/lavasponge/blocks/BlockRegister.java
@@ -3,6 +3,7 @@ package uno.rebellious.lavasponge.blocks;
 import net.minecraft.world.item.BlockItem;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.crafting.RecipeType;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.SoundType;
 import net.minecraft.world.level.block.state.BlockBehaviour;
@@ -14,6 +15,8 @@ import net.minecraftforge.registries.ForgeRegistries;
 import uno.rebellious.lavasponge.LavaSponge;
 import uno.rebellious.lavasponge.config.Config;
 
+import java.util.List;
+
 public class BlockRegister {
     private static final DeferredRegister<Block> BLOCKS = DeferredRegister.create(ForgeRegistries.BLOCKS, LavaSponge.MODID);
     public static final RegistryObject<LavaSpongeBlock> LAVA_SPONGE = BLOCKS.register("lavasponge", () -> new LavaSpongeBlock(BlockBehaviour.Properties.of(Material.SPONGE).strength(0.6F).sound(SoundType.METAL)));
@@ -21,14 +24,19 @@ public class BlockRegister {
     private static final DeferredRegister<Item> ITEMS = DeferredRegister.create(ForgeRegistries.ITEMS, LavaSponge.MODID);
     public static final RegistryObject<Item> LAVA_SPONGE_ITEM = ITEMS.register("lavasponge", () -> new BlockItem(LAVA_SPONGE.get(), new Item.Properties().tab(Config.ITEM_GROUP)));
     public static final RegistryObject<Item> HOT_LAVA_SPONGE_ITEM = ITEMS.register("hot_lavasponge", () -> new BlockItem(HOT_LAVA_SPONGE.get(), new Item.Properties().tab(Config.ITEM_GROUP).fireResistant().stacksTo(1).craftRemainder(LAVA_SPONGE_ITEM.get())) {
-        @Override
-        public int getBurnTime(ItemStack itemStack) {
-            return 16000;
-        }
-    });
 
-    public static void registerBlocks() {
-        BLOCKS.register(FMLJavaModLoadingContext.get().getModEventBus());
-        ITEMS.register(FMLJavaModLoadingContext.get().getModEventBus());
+        @Override
+        public int getBurnTime(ItemStack stack, RecipeType recipeType) {
+            if (List.of(RecipeType.SMELTING, RecipeType.BLASTING, RecipeType.SMOKING).contains(recipeType)) {
+                return 16000;
+            }
+            return -1;
+        }});
+
+
+        public static void registerBlocks() {
+            BLOCKS.register(FMLJavaModLoadingContext.get().getModEventBus());
+            ITEMS.register(FMLJavaModLoadingContext.get().getModEventBus());
+
+        }
     }
-}
