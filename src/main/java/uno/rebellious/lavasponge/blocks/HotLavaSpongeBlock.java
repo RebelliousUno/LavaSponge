9035534a29a85ec78cbd2dea235e179edf10a736
RebelliousUno <1047313+RebelliousUno@users.noreply.github.com>
2021-07-25 11:29:09 +0100

    Update Around Forge and Gradle Versions

--- src/main/java/uno/rebellious/lavasponge/blocks/HotLavaSpongeBlock.java
+++ src/main/java/uno/rebellious/lavasponge/blocks/HotLavaSpongeBlock.java
@@ -62,37 +62,38 @@ public class HotLavaSpongeBlock extends Block {
     @Override
     public void animateTick(BlockState stateIn, World worldIn, BlockPos pos, Random rand) {
         Direction direction = Direction.getRandom(rand);
-        if (direction != Direction.UP) {
-            BlockPos blockpos = pos.relative(direction);
-            BlockState blockstate = worldIn.getBlockState(blockpos);
-            if (!stateIn.canOcclude() || !blockstate.isFaceSturdy(worldIn, blockpos, direction.getOpposite())) {
-                double d0 = pos.getX();
-                double d1 = pos.getY();
-                double d2 = pos.getZ();
-                if (direction == Direction.DOWN) {
-                    d1 = d1 - 0.05D;
-                    d0 += rand.nextDouble();
-                    d2 += rand.nextDouble();
-                } else {
-                    d1 = d1 + rand.nextDouble() * 0.8D;
-                    if (direction.getAxis() == Direction.Axis.X) {
-                        d2 += rand.nextDouble();
-                        if (direction == Direction.EAST) {
-                            ++d0;
-                        } else {
-                            d0 += 0.05D;
-                        }
+        if (direction == Direction.UP) return;
+
+        BlockPos blockpos = pos.relative(direction);
+        BlockState blockstate = worldIn.getBlockState(blockpos);
+
+        if (!stateIn.canOcclude() || !blockstate.isFaceSturdy(worldIn, blockpos, direction.getOpposite())) {
+            double dripX = pos.getX();
+            double dripY = pos.getY();
+            double dripZ = pos.getZ();
+            if (direction == Direction.DOWN) {
+                dripY -= 0.05D; //Start drip at a slight below Y
+                dripX += rand.nextDouble(); //Random X
+                dripZ += rand.nextDouble(); //Random Z
+            } else {
+                dripY += (rand.nextDouble() * 0.8D); // Y + random/8
+                if (direction.getAxis() == Direction.Axis.X) { // if EAST West
+                    dripZ += rand.nextDouble(); // Randomise the Z position (
+                    if (direction == Direction.EAST) {
+                        ++dripX;
                     } else {
-                        d0 += rand.nextDouble();
-                        if (direction == Direction.SOUTH) {
-                            ++d2;
-                        } else {
-                            d2 += 0.05D;
-                        }
+                        dripX += 0.05D;  //if West increment X by a bit
+                    }
+                } else { //AXIS is Z (So NORTH/SOUTH)
+                    dripX += rand.nextDouble();
+                    if (direction == Direction.SOUTH) {
+                        ++dripZ; // Decrement Z if South
+                    } else {
+                        dripZ += 0.05D; // Increment Z if North
                     }
                 }
-                worldIn.addParticle(ParticleTypes.DRIPPING_LAVA, d0, d1, d2, 0.0D, 0.0D, 0.0D);
             }
+            worldIn.addParticle(ParticleTypes.DRIPPING_LAVA, dripX, dripY, dripZ, 0.0D, 0.0D, 0.0D);
         }
     }
 }
