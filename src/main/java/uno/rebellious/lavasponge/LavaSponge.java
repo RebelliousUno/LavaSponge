2c29a49addbd4e9f696e372b7ed25e3773cbc2f8
RebelliousUno <1047313+RebelliousUno@users.noreply.github.com>
2022-04-17 12:23:17 +0100

    Feature(Config): Add Config for World Gen

--- src/main/java/uno/rebellious/lavasponge/LavaSponge.java
+++ src/main/java/uno/rebellious/lavasponge/LavaSponge.java
@@ -1,13 +1,13 @@
 package uno.rebellious.lavasponge;
 
-import net.minecraftforge.common.MinecraftForge;
-import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.fml.ModLoadingContext;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import uno.rebellious.lavasponge.blocks.BlockRegister;
-import uno.rebellious.lavasponge.worldgeneration.WorldGen;
+import uno.rebellious.lavasponge.config.LavaSpongeConfig;
+import net.minecraftforge.fml.config.ModConfig;
 
 @Mod(LavaSponge.MODID)
 public class LavaSponge {
@@ -19,7 +19,7 @@ public class LavaSponge {
     public LavaSponge() {
         BlockRegister.registerBlocks();
         ModSetup.setup();
-
+        ModLoadingContext.get().registerConfig(ModConfig.Type.SERVER, LavaSpongeConfig.SERVER_CONFIG);
         FMLJavaModLoadingContext.get().getModEventBus().addListener(ModSetup::init);
     }
 
