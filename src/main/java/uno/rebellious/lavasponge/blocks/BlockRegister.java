bdab09ecaf1bca1ed80dcacc55ae0d62d5355038
RebelliousUno <1047313+RebelliousUno@users.noreply.github.com>
2023-07-15 17:31:34 +0100

    feat(): Update to 1.20.1

--- src/main/java/uno/rebellious/lavasponge/blocks/BlockRegister.java
+++ src/main/java/uno/rebellious/lavasponge/blocks/BlockRegister.java
@@ -1,16 +1,16 @@
 package uno.rebellious.lavasponge.blocks;
 
+import net.minecraft.core.registries.Registries;
+import net.minecraft.network.chat.Component;
+import net.minecraft.network.chat.contents.TranslatableContents;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.tags.ItemTags;
 import net.minecraft.tags.TagKey;
-import net.minecraft.world.item.BlockItem;
-import net.minecraft.world.item.Item;
-import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.*;
 import net.minecraft.world.item.crafting.RecipeType;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.SoundType;
 import net.minecraft.world.level.block.state.BlockBehaviour;
-import net.minecraft.world.level.material.Material;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
@@ -20,12 +20,10 @@ import uno.rebellious.lavasponge.LavaSponge;
 
 public class BlockRegister {
 
-//    private static final DeferredRegister<GlobalLootModifierSerializer<?>> LOOT = DeferredRegister.create(ForgeRegistries.LOOT_MODIFIER_SERIALIZERS,LavaSponge.MODID);
-
     public static final TagKey<Item> ICE_ITEM_TAG = ItemTags.create(new ResourceLocation(LavaSponge.MODID, "lavaspongecooler"));
     private static final DeferredRegister<Block> BLOCKS = DeferredRegister.create(ForgeRegistries.BLOCKS, LavaSponge.MODID);
-    public static final RegistryObject<LavaSpongeBlock> LAVA_SPONGE = BLOCKS.register("lavasponge", () -> new LavaSpongeBlock(BlockBehaviour.Properties.of(Material.SPONGE).strength(0.6F).sound(SoundType.METAL)));
-    public static final RegistryObject<HotLavaSpongeBlock> HOT_LAVA_SPONGE = BLOCKS.register("hot_lavasponge", () -> new HotLavaSpongeBlock(BlockBehaviour.Properties.of(Material.SPONGE).strength(0.6F).sound(SoundType.METAL).lightLevel((lightValue) -> 14)));
+    public static final RegistryObject<LavaSpongeBlock> LAVA_SPONGE = BLOCKS.register("lavasponge", () -> new LavaSpongeBlock(BlockBehaviour.Properties.of().strength(0.6F).sound(SoundType.METAL)));
+    public static final RegistryObject<HotLavaSpongeBlock> HOT_LAVA_SPONGE = BLOCKS.register("hot_lavasponge", () -> new HotLavaSpongeBlock(BlockBehaviour.Properties.of().strength(0.6F).sound(SoundType.METAL).lightLevel((lightValue) -> 14)));
     private static final DeferredRegister<Item> ITEMS = DeferredRegister.create(ForgeRegistries.ITEMS, LavaSponge.MODID);
     public static final RegistryObject<Item> LAVA_SPONGE_ITEM = ITEMS.register("lavasponge", () -> new BlockItem(LAVA_SPONGE.get(), new Item.Properties()));
     public static final RegistryObject<Item> HOT_LAVA_SPONGE_ITEM = ITEMS.register("hot_lavasponge", () -> new BlockItem(HOT_LAVA_SPONGE.get(), new Item.Properties().fireResistant().stacksTo(1).craftRemainder(LAVA_SPONGE_ITEM.get())) {
@@ -33,17 +31,24 @@ public class BlockRegister {
         public int getBurnTime(ItemStack stack, RecipeType recipeType) {
             return 16000;
         }
-
-//        @Override
-//        public void onDestroyed(ItemEntity itemEntity, DamageSource damageSource) {
-//            System.out.println("Item Destroyed");
-//        }
     });
 
+    public static final DeferredRegister<CreativeModeTab> CREATIVE_MODE_TABS = DeferredRegister.create(Registries.CREATIVE_MODE_TAB, LavaSponge.MODID);
+    public static final RegistryObject<CreativeModeTab> LAVASPONGE_TAB = CREATIVE_MODE_TABS.register("lavasponge_tab", () -> CreativeModeTab.builder()
+            .withTabsBefore(CreativeModeTabs.FUNCTIONAL_BLOCKS)
+            .icon(() -> HOT_LAVA_SPONGE_ITEM.get().getDefaultInstance())
+            .displayItems((parameters, output) -> {
+                output.accept(HOT_LAVA_SPONGE_ITEM.get());
+                output.accept(LAVA_SPONGE_ITEM.get());
+            })
+            .title(Component.translatable("item_group.lavasponge.item_group"))
+            .build());
     public static void registerBlocks() {
         var bus = FMLJavaModLoadingContext.get().getModEventBus();
         BLOCKS.register(bus);
         ITEMS.register(bus);
+        CREATIVE_MODE_TABS.register(bus);
+
     }
 
 
--- src/main/java/uno/rebellious/lavasponge/generators/LootTableHelper.java
+++ src/main/java/uno/rebellious/lavasponge/generators/LootTableHelper.java
@@ -1,14 +1,10 @@
 package uno.rebellious.lavasponge.generators;
 
-import com.google.gson.Gson;
-import com.google.gson.GsonBuilder;
+
 import net.minecraft.advancements.critereon.EnchantmentPredicate;
 import net.minecraft.advancements.critereon.ItemPredicate;
 import net.minecraft.advancements.critereon.MinMaxBounds;
-import net.minecraft.data.CachedOutput;
-import net.minecraft.data.DataGenerator;
-import net.minecraft.data.DataProvider;
-import net.minecraft.data.loot.LootTableProvider;
+
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.enchantment.Enchantments;
@@ -16,23 +12,16 @@ import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.entity.BlockEntityType;
 import net.minecraft.world.level.storage.loot.LootPool;
 import net.minecraft.world.level.storage.loot.LootTable;
-import net.minecraft.world.level.storage.loot.LootTables;
+
 import net.minecraft.world.level.storage.loot.entries.AlternativesEntry;
 import net.minecraft.world.level.storage.loot.entries.DynamicLoot;
 import net.minecraft.world.level.storage.loot.entries.LootItem;
 import net.minecraft.world.level.storage.loot.functions.*;
-import net.minecraft.world.level.storage.loot.parameters.LootContextParamSets;
 import net.minecraft.world.level.storage.loot.predicates.MatchTool;
 import net.minecraft.world.level.storage.loot.providers.nbt.ContextNbtProvider;
 import net.minecraft.world.level.storage.loot.providers.number.ConstantValue;
 import net.minecraft.world.level.storage.loot.providers.number.UniformGenerator;
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
 
-import java.io.IOException;
-import java.nio.file.Path;
-import java.util.HashMap;
-import java.util.Map;
 
 /*
 From McJty Tutorials
