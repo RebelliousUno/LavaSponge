bb8ac16903c82eef573bf2c2b976604d798480ab
RebelliousUno <1047313+RebelliousUno@users.noreply.github.com>
2023-07-27 08:10:33 +0100

    fix(#115): Fix for loot table on 1.20.1

--- src/main/java/uno/rebellious/lavasponge/generators/LavaSpongeLootTables.java
+++ src/main/java/uno/rebellious/lavasponge/generators/LavaSpongeLootTables.java
@@ -1,21 +1,25 @@
 package uno.rebellious.lavasponge.generators;
 
-import net.minecraft.data.DataGenerator;
-import net.minecraft.data.loot.LootTableSubProvider;
-import net.minecraft.resources.ResourceLocation;
-import net.minecraft.world.level.storage.loot.LootTable;
+import net.minecraft.data.loot.packs.VanillaBlockLoot;
+import net.minecraftforge.registries.ForgeRegistries;
+import uno.rebellious.lavasponge.LavaSponge;
 import uno.rebellious.lavasponge.blocks.BlockRegister;
 
-import java.util.function.BiConsumer;
-
-import static uno.rebellious.lavasponge.generators.LootTableHelper.createSimpleTable;
-
-public class LavaSpongeLootTables implements LootTableSubProvider {
+import java.util.Map;
+import java.util.stream.Collectors;
+public class LavaSpongeLootTables extends VanillaBlockLoot {
 
     @Override
-    public void generate(BiConsumer<ResourceLocation, LootTable.Builder> builder) {
-        builder.accept(BlockRegister.HOT_LAVA_SPONGE.getId(), createSimpleTable("hot_lavasponge", BlockRegister.HOT_LAVA_SPONGE.get()));
-        builder.accept(BlockRegister.LAVA_SPONGE.getId(), createSimpleTable("lavasponge", BlockRegister.LAVA_SPONGE.get()));
+    protected void generate() {
+        dropSelf(BlockRegister.HOT_LAVA_SPONGE.get());
+        dropSelf(BlockRegister.LAVA_SPONGE.get());
+    }
+    @Override
+    protected Iterable getKnownBlocks() {
+        return ForgeRegistries.BLOCKS.getEntries().stream()
+                .filter(e -> e.getKey().location().getNamespace().equals(LavaSponge.MODID))
+                .map(Map.Entry::getValue)
+                .collect(Collectors.toList());
     }
 }
 
